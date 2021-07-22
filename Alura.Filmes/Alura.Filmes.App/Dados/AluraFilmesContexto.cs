c9277362fc738678fdd7ea3534508b2a60aebff6
Danielly Primo <60229135+dan-primo@users.noreply.github.com>
2021-07-21 21:44:18 -0300

    mapeamento

---
+++ Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,14 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-O2VHG4H;Database=AluraFilmes;Trusted_connection=true;");
+        }
+    }
+}
--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            //select * from actor
+            using (var contexto = new AluraFilmesContexto())
+            {
+                foreach (var ator in contexto.Atores)
+                {
+                    System.Console.WriteLine(ator);
+                }
+            }
         }
     }
 }
